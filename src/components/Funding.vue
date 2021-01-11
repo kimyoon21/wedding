fac6646afee9e0fdff4e8f1e09ade64399b1c210
김윤혁 <kimyoon21@gmail.com>
2021-01-12 00:20:44 +0900

    멘트 수정

--- src/components/Funding.vue
+++ src/components/Funding.vue
@@ -2,7 +2,7 @@
   <div class="funding">
     <img src="../assets/images/hands.png" class="hands" />
     <h2 class="h2">
-      둘째, 기부를 통해 행복한 날을 축하하기
+      둘째, 기부를 통해 뜻깊은 날로 만들기
     </h2>
     <p>
       인생에서 가장 행복한 날,<br />
@@ -13,9 +13,11 @@
       저희를 축하해주고 싶은 그 마음을<br />
       장기화된 코로나로 인해<br />
       끼니를 해결하지 못하는 아이들에게<br />
-      따뜻한 한 끼로 나누어주세요.
+      따뜻한 한 끼로 나누어주세요.<br />
+      감사한 마음 잘 모아서 전하고<br/>
+      나누어주신 만큼 더욱 행복하게 살겠습니다.<br/>
     </p>
-    <Button @click="funding">기부하기</Button>
+    <Button @click="funding">기부금 전달하기</Button>
 
     <a
       href="https://www.kfhi.or.kr/apps/campaign/2020_stophunger_domestic_2.asp?id=540&mcode=658&utm_medium=display_ad&utm_source=youtube&utm_campaign=2020_stophunger_domestic_2&gclid=Cj0KCQiA5bz-BRD-ARIsABjT4nieWe1teUo7KT5HSTinY-IG9KFVj7BRfzI7I_YYAqapQ4jQFsqEqYIaAvqqEALw_wcB"
